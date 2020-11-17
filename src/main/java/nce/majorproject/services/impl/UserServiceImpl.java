3bd796e6977cd0fbdf2e3b68bb85f6c460e96616
Asmin Shrestha <shrestha.asmin17@gmail.com>
2020-11-17 18:58:44 +0545

    modified user registration response

--- src/main/java/nce/majorproject/services/impl/UserServiceImpl.java
+++ src/main/java/nce/majorproject/services/impl/UserServiceImpl.java
@@ -1,20 +1,26 @@
 package nce.majorproject.services.impl;
 
+import nce.majorproject.constant.UserType;
 import nce.majorproject.context.ContextHolderServices;
 import nce.majorproject.dto.Response;
 import nce.majorproject.dto.UserAddRequest;
+import nce.majorproject.dto.UserAddResponse;
 import nce.majorproject.dto.UserProfileResponse;
 import nce.majorproject.entities.User;
 import nce.majorproject.exception.RestException;
 import nce.majorproject.repositories.UserRepository;
 import nce.majorproject.services.UserService;
 import nce.majorproject.util.DateUtil;
+import nce.majorproject.util.JwtTokenUtil;
 import nce.majorproject.util.SecurityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -22,21 +28,52 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final ContextHolderServices contextHolderServices;
+    private final JwtTokenUtil jwtTokenUtil;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository,ContextHolderServices contextHolderServices) {
+    public UserServiceImpl(UserRepository userRepository,
+                           ContextHolderServices contextHolderServices,
+                           JwtTokenUtil jwtTokenUtil) {
         this.userRepository = userRepository;
         this.contextHolderServices=contextHolderServices;
+        this.jwtTokenUtil = jwtTokenUtil;
     }
 
+    @Transactional
     @Override
-    public Response addUser(UserAddRequest userAddRequest) {
+    public UserAddResponse addUser(UserAddRequest userAddRequest) {
        verifyUserAge(DateUtil.stringToDate(userAddRequest.getDob()));
+       verifyDuplicateUserName(userAddRequest.getUserName());
        User user=prepareUserAddRequest(userAddRequest);
+       user.setLoginTime(LocalDateTime.now());
        User response=userRepository.save(user);
-       return Response.builder().status("SUCCESS").id(response.getId()).build();
+        final String token= jwtTokenUtil.generateToken(this.prepareClaims(user.getUserName(),user.getId(), UserType.User.name()));
+       return UserAddResponse.builder().accessToken(token)
+                .id(response.getId())
+                .addedDate(response.getAddedDate())
+                .address(response.getAddress())
+                .dob(response.getDob())
+                .fullName(response.getFullName())
+                .gender(response.getGender())
+                .loginTime(response.getLoginTime())
+                .phone(response.getPhone())
+                .userName(response.getUserName())
+                .build();
     }
-private void verifyUserAge(LocalDate dob){
+    private void verifyDuplicateUserName(String userName){
+        if(userRepository.validateUserName(userName).isPresent()){
+            throw new RestException("UserName is already taken");
+        }
+    }
+    private Map<String, Object> prepareClaims(String userName, Long id, String type) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", id);
+        claims.put("userName", userName);
+        claims.put("userType", type);
+        return claims;
+    }
+
+    private void verifyUserAge(LocalDate dob){
         if(dob.isAfter(LocalDate.now().minusYears(13))){
             throw new RestException("Your age should be greater than 13");
         }
