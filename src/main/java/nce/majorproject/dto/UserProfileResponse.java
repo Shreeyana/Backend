b83d4da4c27fb4cf6c2dfd3c4825f3146d46affa
Asmin Shrestha <shrestha.asmin17@gmail.com>
2020-11-17 21:25:13 +0545

    profile api extra responses added

--- src/main/java/nce/majorproject/dto/UserProfileResponse.java
+++ src/main/java/nce/majorproject/dto/UserProfileResponse.java
@@ -3,12 +3,20 @@ package nce.majorproject.dto;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Getter
 @Builder
 public class UserProfileResponse {
 
+    private Long id;
+    private String fullName;
     private String userName;
-    private Long userId;
-    private String gender;
     private String address;
+    private LocalDate dob;
+    private String gender;
+    private String phone;
+    private LocalDateTime addedDate;
+    private LocalDateTime loginTime;
 }
--- src/main/java/nce/majorproject/services/AdminServices.java
+++ src/main/java/nce/majorproject/services/AdminServices.java
@@ -2,7 +2,6 @@ package nce.majorproject.services;
 
 import nce.majorproject.dto.AdminRegisterRequest;
 import nce.majorproject.dto.Response;
-import nce.majorproject.dto.UserProfileResponse;
 import nce.majorproject.entities.User;
 
 import java.util.List;
